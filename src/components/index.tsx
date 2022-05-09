0275aba6aa6f0342c59368ac18450a461ebd09c7
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-05-09 20:50:36 -0300

    feat(setup): adjusting components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
-export {default as Login} from './Login'
-export {default as Logout} from './Logout'
-export {default as ResultCard} from './ResultCard'
-export {default as SearchArea} from './SearchArea'
+export { default as Login } from "./Login";
+export { default as Logout } from "./Logout";
+export { default as ResultCard } from "./ResultCard";
+export { default as SearchForm } from "./SearchForm";
